9992d58fc04c3e7f94162d83e8b5cb6cb400b98e
Steven Braham <steven@braham.biz>
2017-02-11 18:34:46 +0100

    added some algortihm

--- source/js/lib/HandsDetector.js
+++ source/js/lib/HandsDetector.js
@@ -8,20 +8,68 @@
 class HandsDetector {
 
     /**
-     *
-     * @param height
-     * @param width
+     * Cards should be given in the cardsJS format
+     * eg. Heart ace => AH, 8 club => 8C
+     * @param cards
      */
-    constructor(height, width) {
-        this.height = height;
-        this.width = width;
+    constructor(cards) {
+        this.cardsCount = cards.length;
+        this.cards = {};
+        //split suit and value
+        for (var i = 0, len = cards.length; i < len; i++) {
+            let splitted = cards[i].split("");
+            //splitted[1] = the suit
+            if (this.cards[splitted[0]]) {
+                this.cards[splitted[0]].push(splitted[1]);
+            } else {
+                this.cards[splitted[0]] = [splitted[1]];
+            }
+        }
     }
 
-    sayHi(message) {
-        return 4;
+    isOnePair() {
+        return this.hasPairs(2, 1);
     }
 
+    isTwoPair() {
+        return this.hasPairs(2, 2);
+    }
 
+    isThreeOfAKind() {
+        return this.hasPairs(3, 1);
+    }
+
+    isFourOfAKind() {
+        return this.hasPairs(4, 1);
+    }
+
+    isARoyalHouse() {
+        return (this.isThreeOfAKind() && this.isOnePair() && this.cardsCount >= 5);
+    }
+
+    /**
+     * Check if a pair of values in present
+     * @param {Number} cardsInPair how many cards are a pair
+     * @param {Number} pairsNeeded how many pairs should be found
+     * @return {bool}
+     */
+    hasPairs(cardsInPair, pairsNeeded) {
+        if (this.cardsCount < cardsInPair) {
+            return false;
+        }
+        let pairsFound = 0;
+        for (let cardValue in this.cards) {
+            if (this.cards.hasOwnProperty(cardValue)) {
+                if (this.cards[cardValue].length >= cardsInPair) {
+                    pairsFound++;
+                    if (pairsFound >= pairsNeeded) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
 }
 
 module.exports = HandsDetector;
--- spec/tests/handetector-spec.js
+++ spec/tests/handetector-spec.js
@@ -1,11 +1,42 @@
 /**
  * Created by stevenbraham on 11-02-17.
  */
-
-import HandsDetector from '../../source/js/lib/HandsDetector';
+import HandsDetector from "../../source/js/lib/HandsDetector";
 
 describe("Handsdetector suite", function () {
-    it("contains spec with an expectation", function () {
-        expect(new HandsDetector().sayHi()).toBe(34);
+    it("Tests the one pair function", function () {
+        expect(new HandsDetector(['5C', '5H', 'KH', 'QC']).isOnePair()).toBe(true);
+        expect(new HandsDetector(['7C', '5H', '2H', 'QC']).isOnePair()).toBe(false);
+        expect(new HandsDetector(['QH', '7C', '5H', 'QC']).isOnePair()).toBe(true);
+    });
+
+    it("Tests the two pair function", function () {
+        expect(new HandsDetector(['5C', '5H', 'KH', 'QC']).isTwoPair()).toBe(false);
+        expect(new HandsDetector(['7C', '5H', '2H', 'QC']).isTwoPair()).toBe(false);
+        expect(new HandsDetector(['7C', '7S', '2H', '2C', 'QS']).isTwoPair()).toBe(true);
+        expect(new HandsDetector(['QH', '7C', '5H', 'QC']).isTwoPair()).toBe(false);
+    });
+
+    it("Tests the 3 of a kind function", function () {
+        expect(new HandsDetector(['5C', '5H', 'KH', 'QC']).isThreeOfAKind()).toBe(false);
+        expect(new HandsDetector(['7C', '5H', '2H', 'QC']).isThreeOfAKind()).toBe(false);
+        expect(new HandsDetector(['7C', '7H', '7S', 'QC']).isThreeOfAKind()).toBe(true);
+        expect(new HandsDetector(['7C', '7S', '2H', '2C', 'QS']).isThreeOfAKind()).toBe(false);
+        expect(new HandsDetector(['QH', '7C', '5H', 'QC']).isThreeOfAKind()).toBe(false);
+    });
+
+    it("Tests the 4 of a kind function", function () {
+        expect(new HandsDetector(['5C', '5H', 'KH', 'QC']).isFourOfAKind()).toBe(false);
+        expect(new HandsDetector(['7C', '5H', '2H', 'QC']).isFourOfAKind()).toBe(false);
+        expect(new HandsDetector(['7C', '7H', '7S', 'QC']).isFourOfAKind()).toBe(false);
+        expect(new HandsDetector(['7C', '7S', '7H', '7D', 'QS']).isFourOfAKind()).toBe(true);
+        expect(new HandsDetector(['QH', '7C', '5H', 'QC']).isFourOfAKind()).toBe(false);
+    });
+
+    it("Tests the full house function function", function () {
+        expect(new HandsDetector(['5C', '5H', 'KH', 'QC']).isARoyalHouse()).toBe(false);
+        expect(new HandsDetector(['7C', '5H', '2H', 'QC']).isARoyalHouse()).toBe(false);
+        expect(new HandsDetector(['AC', '7S', '7H', '7D', 'QS']).isARoyalHouse()).toBe(false);
+        expect(new HandsDetector(['7C', '7S', '7H', 'QD', 'QS']).isARoyalHouse()).toBe(true);
     });
 });
