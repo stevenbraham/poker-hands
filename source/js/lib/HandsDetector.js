8d74f9aa1d5e17aa03372c4a11a351f4fd4d79c3
Steven Braham <steven@braham.biz>
2017-02-12 19:07:30 +0100

    added some algortihm

--- source/js/lib/HandsDetector.js
+++ source/js/lib/HandsDetector.js
@@ -14,15 +14,22 @@ class HandsDetector {
      */
     constructor(cards) {
         this.cardsCount = cards.length;
-        this.cards = {};
+        this.cardsByValue = {};
+        this.cardsBySuit = {};
         //split suit and value
         for (var i = 0, len = cards.length; i < len; i++) {
             let splitted = cards[i].split("");
             //splitted[1] = the suit
-            if (this.cards[splitted[0]]) {
-                this.cards[splitted[0]].push(splitted[1]);
+            if (this.cardsByValue[splitted[0]]) {
+                this.cardsByValue[splitted[0]].push(splitted[1]);
             } else {
-                this.cards[splitted[0]] = [splitted[1]];
+                this.cardsByValue[splitted[0]] = [splitted[1]];
+            }
+
+            if (this.cardsBySuit[splitted[1]]) {
+                this.cardsBySuit[splitted[1]].push(splitted[0]);
+            } else {
+                this.cardsBySuit[splitted[1]] = [splitted[0]];
             }
         }
     }
@@ -43,24 +50,44 @@ class HandsDetector {
         return this.hasPairs(4, 1);
     }
 
-    isARoyalHouse() {
-        return (this.isThreeOfAKind() && this.isOnePair() && this.cardsCount >= 5);
+    isAFullHouse() {
+        return (this.hasPairs(3, 1, false) && this.hasPairs(2, 1, false) && this.cardsCount >= 5);
+    }
+
+    isFlush() {
+        for (let suit in this.cardsBySuit) {
+            if (this.cardsBySuit.hasOwnProperty(suit)) {
+                if (this.cardsBySuit[suit].length == this.cardsCount) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    isStraight() {
+        if (this.cardsCount > 1) {
+
+        }
+        return false;
     }
 
     /**
      * Check if a pair of values in present
      * @param {Number} cardsInPair how many cards are a pair
      * @param {Number} pairsNeeded how many pairs should be found
+     * @param {bool} Should the comparison be == or >=
      * @return {bool}
      */
-    hasPairs(cardsInPair, pairsNeeded) {
+    hasPairs(cardsInPair, pairsNeeded, allowGreaterThan = true) {
         if (this.cardsCount < cardsInPair) {
             return false;
         }
         let pairsFound = 0;
-        for (let cardValue in this.cards) {
-            if (this.cards.hasOwnProperty(cardValue)) {
-                if (this.cards[cardValue].length >= cardsInPair) {
+        for (let cardValue in this.cardsByValue) {
+            if (this.cardsByValue.hasOwnProperty(cardValue)) {
+                let passedCheck = allowGreaterThan ? (this.cardsByValue[cardValue].length >= cardsInPair) : this.cardsByValue[cardValue].length == cardsInPair;
+                if (passedCheck) {
                     pairsFound++;
                     if (pairsFound >= pairsNeeded) {
                         return true;
--- spec/tests/handetector-spec.js
+++ spec/tests/handetector-spec.js
@@ -8,6 +8,7 @@ describe("Handsdetector suite", function () {
         expect(new HandsDetector(['5C', '5H', 'KH', 'QC']).isOnePair()).toBe(true);
         expect(new HandsDetector(['7C', '5H', '2H', 'QC']).isOnePair()).toBe(false);
         expect(new HandsDetector(['QH', '7C', '5H', 'QC']).isOnePair()).toBe(true);
+        expect(new HandsDetector(['QH', 'QS', '5H', 'QC']).isOnePair()).toBe(true);
     });
 
     it("Tests the two pair function", function () {
@@ -34,9 +35,26 @@ describe("Handsdetector suite", function () {
     });
 
     it("Tests the full house function function", function () {
-        expect(new HandsDetector(['5C', '5H', 'KH', 'QC']).isARoyalHouse()).toBe(false);
-        expect(new HandsDetector(['7C', '5H', '2H', 'QC']).isARoyalHouse()).toBe(false);
-        expect(new HandsDetector(['AC', '7S', '7H', '7D', 'QS']).isARoyalHouse()).toBe(false);
-        expect(new HandsDetector(['7C', '7S', '7H', 'QD', 'QS']).isARoyalHouse()).toBe(true);
+        expect(new HandsDetector(['5C', '5H', 'KH', 'QC']).isAFullHouse()).toBe(false);
+        expect(new HandsDetector(['7C', '5H', '2H', 'QC']).isAFullHouse()).toBe(false);
+        expect(new HandsDetector(['AC', '7S', '7H', '7D', 'QS']).isAFullHouse()).toBe(false);
+        expect(new HandsDetector(['7C', '7S', '7H', 'KD', 'QD']).isAFullHouse()).toBe(false);
+        expect(new HandsDetector(['7C', '7S', '7H', 'QD', 'QS']).isAFullHouse()).toBe(true);
+        expect(new HandsDetector(['AC', 'AH', 'AD', 'KH', 'KS']).isAFullHouse()).toBe(true);
+    });
+
+    it("Tests the flush function", function () {
+        expect(new HandsDetector(['5C', '5H', 'KH', 'QC']).isFlush()).toBe(false);
+        expect(new HandsDetector(['4H', '5H', '2H', '3H']).isFlush()).toBe(true);
+        expect(new HandsDetector(['7C', '5H', '2H', 'QC']).isFlush()).toBe(false);
+        expect(new HandsDetector(['AC', '7S', '7H', '7D', 'QS']).isFlush()).toBe(false);
+    });
+
+    it("Tests the straight function", function () {
+        expect(new HandsDetector(['AC', 'KC', 'QK', 'JS']).isStraight()).toBe(true);
+        expect(new HandsDetector(['4H', '5S', '2H', '3C']).isStraight()).toBe(true);
+        expect(new HandsDetector(['7C', '5H', '2H', 'QC']).isStraight()).toBe(false);
+        expect(new HandsDetector(['AC', '7S', '7H', '7D', 'QS']).isStraight()).toBe(false);
+        expect(new HandsDetector(['AC', '2C', '3C', '4C', '6C']).isStraight()).toBe(false);
     });
 });
